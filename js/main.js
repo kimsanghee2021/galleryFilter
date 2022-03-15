68e999b1d78c1c4b6c976ba8061fa1ab9cabdff8
kimsanghee <tkdgmltjddnr@naver.com>
2022-03-15 17:33:30 +0900

    검색 작업 함수 완료

--- js/main.js
+++ js/main.js
@@ -1,8 +1,3 @@
-//for of 반복문으로 제목과 인덱스 번호를 배열로 담기
-//인풋박스에 텍스트 입력시 해당 텍스트 노출하기 
-//모든 사진을 다 안보이게 한다.
-//현재 인풋에 담기는 값과 인풋에 담기는 값에 맞는 필터를 만들어라
-//필터어레이에 0개 이상 즉 1개라도 나오면 해당하는 값을 노출시켜라
 const topWrap = document.querySelector('.toolbar');
 const search = topWrap.querySelector('input[type="search"]');
 const totlePT = topWrap.querySelector('.counter span');
@@ -25,7 +20,7 @@ btns.forEach(function(btn,idx){
             const parent = el.parentElement;
             parent.classList.remove(active);
             btns[idx].parentElement.classList.add(active);
-            // console.log(parent);
+
             if(btns[idx].parentElement.classList.contains('show-list')){
                 btns[idx].parentElement.previousElementSibling.previousElementSibling.classList.add(hidden);
                 imgList.classList.remove(gridView);
@@ -46,3 +41,39 @@ inputRange.addEventListener('input',function(){
 });
 
 //검색키워드 필터 적용방법
+const myArr = [];
+let countIdx = 1;
+//for of 반복문으로 제목과 인덱스 번호를 배열로 담기
+for(const item of imgListItemTT){
+    myArr.push({
+        id:countIdx++,
+        text : item.textContent
+    });
+}
+
+//인풋박스에 텍스트 입력시 해당 텍스트 노출하기 
+search.addEventListener('keyup',handler);
+
+function handler(){
+    //현재 모든 리스트를 숨김처리해라
+    for(const item of imgListItem){
+        item.classList.add(hidden);
+        const keyword = this.value;
+        const arr2 = myArr.filter(function(el){
+            return el.text.includes(keyword);
+        });
+        //console.log(arr2);
+        if(arr2.length > 0){
+            for(const el of arr2){
+                document.querySelector(`.image-list li:nth-child(${el.id})`).classList.remove(hidden);
+            }
+        }
+        totlePT.innerText = arr2.length;
+    }
+
+}
+
+
+
+//현재 인풋에 담기는 값과 인풋에 담기는 값에 맞는 필터를 만들어라
+//필터어레이에 0개 이상 즉 1개라도 나오면 해당하는 값을 노출시켜라
