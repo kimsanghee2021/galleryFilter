2844088fd1629e6d7fb151409f2f1f4c5f71bb1b
kimsanghee <tkdgmltjddnr@naver.com>
2022-03-15 21:12:40 +0900

    수정작업중 es5

--- js/main_es5.js
+++ js/main_es5.js
@@ -1,80 +1,92 @@
-const topWrap = document.querySelector('.toolbar');
-const search = topWrap.querySelector('input[type="search"]');
-const totlePT = topWrap.querySelector('.counter span');
-const btns = topWrap.querySelectorAll('.view-options button');
-const inputRange = topWrap.querySelector('input[type="range"]');
-const imgList = document.querySelector('.image-list');
-const imgListItem = imgList.querySelectorAll('li');
-const imgListItemTT = imgList.querySelectorAll('figcaption p:first-child');
-
-const myArr = [];
-let countIdx = 1;
-
-const active = 'active';
-const gridView = 'grid-view';
-const listView = 'list-view';
-const hidden = 'd-none';
 
 
-btns.forEach(function(btn,idx){
-    btn.addEventListener('click',function(e){
-        e.preventDefault();
-        Btns(btns,idx);
+function Filter(){
+    this.init();
+    this.bindingEvent();
+    this.inputRange01();
+}
+Filter.prototype.init = function(){
+    this.topWrap = document.querySelector('.toolbar');
+    this.search = this.topWrap.querySelector('input[type="search"]');
+    this.totlePT = this.topWrap.querySelector('.counter span');
+    this.btns = this.topWrap.querySelectorAll('.view-options button');
+    this.inputRange = this.topWrap.querySelector('input[type="range"]');
+    this.imgList = document.querySelector('.image-list');
+    this.imgListItem = this.imgList.querySelectorAll('li');
+    this.imgListItemTT = this.imgList.querySelectorAll('figcaption p:first-child');
+    this.myArr = [];
+    this.countIdx = 1;
+    this.active = 'active';
+    this.gridView = 'grid-view';
+    this.listView = 'list-view';
+    this.hidden = 'd-none';
+}
+
+Filter.prototype.bindingEvent = function(){
+    this.btns.forEach(function(btn,idx){
+
+        btn.addEventListener('click', e=>{
+            e.preventDefault();
+
+            this.Btns01(this.btns, idx);
+        });
+    }.bind(this));
+    for(const item of this.imgListItemTT){
+        this.myArr.push({
+            id:this.countIdx++,
+            text : item.textContent
+        });
+    }  
+    this.search.addEventListener('keyup',this.handler);  
+}
+
+
+Filter.prototype.inputRange01 = function(){
+    this.inputRange.addEventListener('input',function(){
+        document.documentElement.style.setProperty('--minRangeValue',`${this.value}px`);
     });
-});
+}
 
 
-inputRange.addEventListener('input',function(){
-    document.documentElement.style.setProperty('--minRangeValue',`${this.value}px`);
-});
-
-
-search.addEventListener('keyup',handler);
 
 //버튼함수
-function Btns(target,idx){
+Filter.prototype.Btns01 = function(target,idx){
     for(const el of target){
         const parent = el.parentElement;
-        parent.classList.remove(active);
-        target[idx].parentElement.classList.add(active);
+        parent.classList.remove(this.active);
+        target[idx].parentElement.classList.add(this.active);
 
         if(target[idx].parentElement.classList.contains('show-list')){
-            target[idx].parentElement.previousElementSibling.previousElementSibling.classList.add(hidden);
-            imgList.classList.remove(gridView);
-            imgList.classList.add(listView);
+            target[idx].parentElement.previousElementSibling.previousElementSibling.classList.add(this.hidden);
+            this.imgList.classList.remove(this.gridView);
+            this.mgList.classList.add(this.listView);
         } else{
-            target[idx].parentElement.previousElementSibling.classList.remove(hidden);
-            imgList.classList.remove(listView);
-            imgList.classList.add(gridView);
+            target[idx].parentElement.previousElementSibling.classList.remove(this.hidden);
+            this.imgList.classList.remove(this.listView);
+            this.imgList.classList.add(this.gridView);
         }
     }
 }
 
 
 
-for(const item of imgListItemTT){
-    myArr.push({
-        id:countIdx++,
-        text : item.textContent
-    });
-}    
 
-
-function handler(){
-    for(const item of imgListItem){
-        item.classList.add(hidden);
+Filter.prototype.handler = function(){
+    for(const item of this.imgListItem){
+        item.classList.add(this.hidden);
         const keyword = this.value;
         const arr2 = myArr.filter(function(el){
             return el.text.includes(keyword);
         });
         if(arr2.length > 0){
             for(const el of arr2){
-                document.querySelector(`.image-list li:nth-child(${el.id})`).classList.remove(hidden);
+                document.querySelector(`.image-list li:nth-child(${el.id})`).classList.remove(this.hidden);
             }
         }
         totlePT.innerText = arr2.length;
     }
 }
+
 
 
 
